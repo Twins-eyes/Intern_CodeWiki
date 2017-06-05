d5d219df233f319d7638b425abfa520ea565e385
whanwhry <myldwhn.77@gmail.com>
2017-06-05 15:32:43 +0900

    nav bar fixed

--- app/components/HomePage.js
+++ app/components/HomePage.js
@@ -4,12 +4,18 @@ import NavBar from '../components/NavBar'
 class HomePage extends Component {
     render () {
         return (
-            <div>
+            <div style={styles.page}>
                 <NavBar location={this.props.location}/>
                 <h1>Home</h1>
             </div>
         )
     }
+}
+
+const styles={
+    page:{
+        backgroundColor: '#F5D773',
+    },
 }
 
 export default HomePage
--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -2,27 +2,34 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
 class NavBar extends Component {
+
+    onMouseOver(){
+        const newStyle = {...styles.link, color:'grey'}
+    }
+
     render () {
         if(this.props.location.pathname == '/'){
             return (
                 <div style={styles.navBar}>
-                    <img src={require('../img/codewikiwhite.gif')} style={styles.logo} />
-                    <Link to={'/signup'}>
-                        Sign up
-                    </Link>
-                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                    <Link to={'/signin'}>
-                        Sign in
-                    </Link>      
+                    <Link to={'/'}><img src={require('../img/codewikiwhite.gif')} style={styles.logo} /></Link>
+                    <span style={styles.menu}>
+                        <Link to={'/signup'} style={styles.link}>
+                            Sign up
+                        </Link>
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <Link to={'/signin'} style={styles.link}>
+                            Sign in
+                        </Link> 
+                    </span>
                 </div>
             )
         } else {
             return (
                 <div style={styles.navBar}>
-                    <Link to={'/'}>
+                    <Link to={'/'} style={styles.link}>
                         {this.props.name}
                     </Link>
-                    <Link to={'/a'}>
+                    <Link to={'/a'} style={styles.link}>
                         Sign out
                     </Link>
                 </div>
@@ -35,14 +42,23 @@ const styles = {
     navBar:{
         height: 75,
         padding: 20,
+        paddingBottom: 0,
         backgroundColor: 'transparent',
-        fontFamily: 'Arial'
+        fontFamily: 'Arial',
+        flexDirection: 'column'
     },
     logo:{
         align: 'left',
-        paddingRight: '860px',
-        width: '200px',
-        height: '50px'
+        width: '120px',
+        height: '25px',
+    },
+    menu:{
+        backgroundColor:'transparent',
+        float: 'right'
+    },
+    link:{
+        color: 'white',
+        textDecoration: 'none'
     }
 }
 
