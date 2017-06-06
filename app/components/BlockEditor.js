aa34e61c0a74c30ae1b628161595727c05da5b09
worachatsun <worachat.sun@mail.kmutt.ac.th>
2017-06-06 16:59:04 +0900

    get selection separate text by using array

--- app/components/BlockEditor.js
+++ app/components/BlockEditor.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {Editor, EditorState, RichUtils} from 'draft-js'
+import {Editor, EditorState, RichUtils, convertToRaw} from 'draft-js'
 import {stateToHTML} from 'draft-js-export-html'
 import { Grid } from 'semantic-ui-react'
 import '../assets/editor.css'
@@ -29,7 +29,27 @@ class BlockEditor extends Component {
         var currentContentBlock = currentContent.getBlockForKey(anchorKey)
         var start = selectionState.getStartOffset()
         var end = selectionState.getEndOffset()
-        var selectedText = currentContentBlock.getText().slice(start, end)
+        let selectedText = currentContentBlock.getText().slice(start, end)
+        
+        if(selectionState.getStartKey() == selectionState.getEndKey()){
+            console.log(selectedText)
+        }else{
+            const rawData = convertToRaw(currentContent).blocks
+            let indexStart = rawData.findIndex(inData => inData.key == selectionState.getStartKey())
+            let indexEnd = rawData.findIndex(inData => inData.key == selectionState.getEndKey())
+
+            let selectionArray = rawData.filter(data => {
+                let lineOfObject = rawData.findIndex(inData => inData.key == data.key)
+                if(lineOfObject === indexStart)
+                    console.log(data.text.slice(start))
+                else if(lineOfObject > indexStart & lineOfObject < indexEnd)
+                    console.log(data.text)
+                else if(lineOfObject === indexEnd)
+                    console.log(data.text.slice(0, end))
+                else
+                    console.log('---------------')
+            })
+        }
     }
 
     _onBoldClick() {
@@ -60,6 +80,11 @@ class BlockEditor extends Component {
         ))
     }
 
+    _onSelectionDescription() {
+        RichUtils.toggleInlineStyle(this.state.editorState, 'HIGHLIGHT')
+        this.setState({editorState: this.state.editorState})
+    }
+
     render() {
         return (
             <div>
@@ -67,7 +92,11 @@ class BlockEditor extends Component {
                     <Grid.Row columns={2}>
                         <Grid.Column>
                             <div className={'editor'}>
-                                <Editor editorState={this.state.editorState} onChange={this.onChange} />
+                                <Editor 
+                                    editorState={this.state.editorState} 
+                                    onChange={this.onChange}
+                                    placeholder={'Tell us something...'}
+                                />
                             </div>
                         </Grid.Column>
                         <Grid.Column>
@@ -78,13 +107,14 @@ class BlockEditor extends Component {
                     </Grid.Row>
                 </Grid>
                 <div>
-                    <div className={'bottonEditor'}>
+                    {/*<div className={'bottonEditor'}>
                         lkmckm
-                    </div>
+                    </div>*/}
                     <button onClick={this._onBoldClick.bind(this)}>BOLD</button>
                     <button onClick={this._onItalicClick.bind(this)}>ITALIC</button>
                     <button onClick={this._onUnderlineClick.bind(this)}>UNDERLINE</button>
                     <button onClick={this._onCodeClick.bind(this)}>CODE</button>
+                    <button onClick={this._onSelectionDescription.bind(this)}>SELECT FOR DESCRIPTION</button>
                 </div>
             </div>
         )
