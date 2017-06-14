daa85db4e0200e6396727c17db9a50bdd9ec2da5
worachatsun <worachat.sun@mail.kmutt.ac.th>
2017-06-14 14:19:15 +0900

    add icon and modified blockEditor

--- app/components/BlockEditor.js
+++ app/components/BlockEditor.js
@@ -1,158 +1,273 @@
 import React, { Component } from 'react'
-import {Editor, EditorState, RichUtils, convertToRaw} from 'draft-js'
-import {stateToHTML} from 'draft-js-export-html'
-import { Row, Col } from 'antd'
+import { connect } from 'react-redux'
+import * as actions from '../actions'
+import { Row, Col, Button, Input } from 'antd'
+import { 
+    CompositeDecorator, 
+    convertToRaw, 
+    Editor, 
+    EditorState, 
+    RichUtils, 
+    ContentState 
+} from 'draft-js'
+import { FaBold, FaItalic, FaUnderline, FaCode } from 'react-icons/lib/fa'
 import '../assets/editor.css'
-//import 'semantic-ui-css/semantic.min.css'
 
 class BlockEditor extends Component {
     constructor(props) {
         super(props)
+
+        const decorator = new CompositeDecorator([
+            {
+                strategy: this.findLinkEntities,
+                component: this.Link.bind(this),
+            },
+        ])
+
         this.state = {
-            editorState: EditorState.createEmpty(),
-            displayEditorState: '',
-            htmlFromEditor: '',
-            content: {},
-            hoverSelection: false
+            editorState: EditorState.createEmpty(decorator),
+            showURLInput: false,
+            urlValue: '',
+            description: ''
         }
-        
-        this.onChange = this.onChange.bind(this)
+
+        this.focus = () => this.refs.editor.focus()
+        this.onChange = (editorState) => this.setState({editorState})
+        this.logState = () => {
+            const content = this.state.editorState.getCurrentContent()
+            console.log(convertToRaw(content))
+        }
+
+        this.promptForLink = this._promptForLink.bind(this)
+        this.onURLChange = (e) => this.setState({urlValue: e.target.value})
+        this.confirmLink = this._confirmLink.bind(this)
+        this.onLinkInputKeyDown = this._onLinkInputKeyDown.bind(this)
+        this.removeLink = this._removeLink.bind(this)
+
     }
 
-    onChange(editorState) {
-        this.setState({
-            editorState,
-            htmlFromEditor: stateToHTML(editorState.getCurrentContent()),
-            //displayEditorState: convertToRaw(editorState.getCurrentContent())
-        })
-        var selectionState = this.state.editorState.getSelection()
-        var anchorKey = selectionState.getAnchorKey()
-        var currentContent = this.state.editorState.getCurrentContent()
-        var currentContentBlock = currentContent.getBlockForKey(anchorKey)
-        var start = selectionState.getStartOffset()
-        var end = selectionState.getEndOffset()
-        let selectedText = currentContentBlock.getText().slice(start, end)
-
-        const rawData = convertToRaw(currentContent).blocks
-        let indexStart = rawData.findIndex(inData => inData.key == selectionState.getStartKey())
-        let indexEnd = rawData.findIndex(inData => inData.key == selectionState.getEndKey())
-        let selectionObj = {}
-        let changedSelectedArray = []
-        let selectionArray = rawData.filter(data => {
-            let lineOfObject = rawData.findIndex(inData => inData.key == data.key)
-            let dataChange = Object.assign({}, data)
-            if(lineOfObject === indexStart){
-                dataChange.text = dataChange.text.slice(start)
-                changedSelectedArray = [...changedSelectedArray, dataChange]
-            }else if(lineOfObject > indexStart & lineOfObject < indexEnd){
-                changedSelectedArray = [...changedSelectedArray, dataChange]
-            }else if(lineOfObject === indexEnd){
-                dataChange.text = dataChange.text.slice(0, end)
-                changedSelectedArray = [...changedSelectedArray, dataChange]
+    _promptForLink(e) {
+        e.preventDefault()
+        const {editorState} = this.state
+        const selection = editorState.getSelection()
+        if (!selection.isCollapsed()) {
+            const contentState = editorState.getCurrentContent()
+            const startKey = editorState.getSelection().getStartKey()
+            const startOffset = editorState.getSelection().getStartOffset()
+            const blockWithLinkAtBeginning = contentState.getBlockForKey(startKey)
+            const linkKey = blockWithLinkAtBeginning.getEntityAt(startOffset)
+            let description = ''
+            if (linkKey) {
+                const linkInstance = contentState.getEntity(linkKey)
+                description = linkInstance.getData().description
             }
+            this.setState({
+                showURLInput: true,
+                urlValue: description,
+            }, () => {
+                setTimeout(() => this.refs.description.focus(), 0)
+            })
+        }
+    }
+
+    _confirmLink(e) {
+        e.preventDefault()
+        const {editorState, urlValue} = this.state
+        const contentState = editorState.getCurrentContent()
+        const contentStateWithEntity = contentState.createEntity(
+            'LINK',
+            'MUTABLE',
+            {description: urlValue}
+        )
+        const entityKey = contentStateWithEntity.getLastCreatedEntityKey()
+        const newEditorState = EditorState.set(editorState, { currentContent: contentStateWithEntity })
+        this.setState({
+        editorState: RichUtils.toggleLink(
+            newEditorState,
+            newEditorState.getSelection(),
+            entityKey
+        ),
+        showURLInput: false,
+        urlValue: '',
+        }, () => {
+            setTimeout(() => this.refs.editor.focus(), 0)
         })
-        console.log(changedSelectedArray)
-        selectionObj = Object.assign(selectionObj, { key: '123' }, { massage: changedSelectedArray }, { description: '' } )
-        console.log(selectionObj)
-        this.setState({ content: selectionObj, hoverSelection: false })
-        console.log(this.state.content)
+    }
+    _onLinkInputKeyDown(e) {
+        if (e.which === 13) {
+            this._confirmLink(e)
+        }
+    }
+    _removeLink(e) {
+        e.preventDefault()
+        const {editorState} = this.state
+        const selection = editorState.getSelection()
+        if (!selection.isCollapsed()) {
+            this.setState({
+                editorState: RichUtils.toggleLink(editorState, selection, null),
+            })
+        }
     }
 
     _onBoldClick() {
-        this.onChange(RichUtils.toggleInlineStyle(
-            this.state.editorState,
-            'BOLD'
-        ))
+        this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'))
     }
 
     _onItalicClick() {
-        this.onChange(RichUtils.toggleInlineStyle(
-            this.state.editorState,
-            'ITALIC'
-        ))
-    }
-
-    _onUnderlineClick() {
-        this.onChange(RichUtils.toggleInlineStyle(
-            this.state.editorState,
-            'UNDERLINE'
-        ))
+        this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'ITALIC'))
     }
 
     _onCodeClick() {
-        this.onChange(RichUtils.toggleInlineStyle(
-            this.state.editorState,
-            'CODE'
-        ))
+        this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'CODE'))
     }
 
-    _onSelectionDescription() {
-        // RichUtils.toggleInlineStyle(this.state.editorState, 'HIGHLIGHT')
-        // this.setState({editorState: this.state.editorState})
-        let content = this.state.content
-        content.bgColor = '#ddd'
-        this.setState({ hoverSelection: true, content })
+    _onUnderlineClick() {
+        this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'UNDERLINE'))
     }
 
-    descriptionTextArea() {
-        if(this.state.hoverSelection){
-            return (
-                <div>
-                    <textarea 
-                        onChange={event => {
-                            let a = this.state.content
-                            a.description = event.target.value
-                            this.setState({content: a})
-                            console.log(this.state.content)
-                        }}
-                        placeholder={'Write some description'}
-                    >
-                    </textarea>
-                </div>
-            )
-        }
+    findLinkEntities(contentBlock, callback, contentState) {
+        contentBlock.findEntityRanges(
+            (character) => {
+                const entityKey = character.getEntity() 
+                return (
+                    entityKey !== null &&
+                    contentState.getEntity(entityKey).getType() === 'LINK'
+                ) 
+            },
+            callback
+        ) 
     }
+
+    Link(props) {
+        const {description} = props.contentState.getEntity(props.entityKey).getData() 
+        return (
+            <code style={styles.link} onMouseOver={() => this.props.changeDescription(description)}>
+                {props.children}
+            </code>
+        ) 
+    }
+
 
     render() {
-        return (
-            <div>
-                <Row gutter={8} style={{margin: 10}}>
-                    <Col span={12} style={{padding: 10}}>
-                        <div className={'editor'} style={{backgroundColor: this.state.content.bgColor}}>
-                            <Editor 
-                                editorState={this.state.editorState} 
-                                onChange={this.onChange}
-                                placeholder={'Tell us something...'}
-                            />
-                        </div>
-                        {this.descriptionTextArea()}
-                    </Col>
-                    <Col span={12} style={{padding: 10}}>
-                        <div className={'editor'}>
-                            {this.state.content.description}
-                            {this.state.htmlFromEditor}
-                        </div>
-                    </Col>
-                </Row>
+        let urlInput
+        if (this.state.showURLInput) {
+        urlInput =
+            <div style={styles.urlInputContainer}>
                 <Row>
-                    <Col span={24} style={{padding: 20}}>
-                        <div dangerouslySetInnerHTML={{__html: this.state.htmlFromEditor}}></div>
-                        <div>
-                            {/*<div className={'bottonEditor'}>
-                                lkmckm
-                            </div>*/}
-                            <button onClick={this._onBoldClick.bind(this)}>BOLD</button>
-                            <button onClick={this._onItalicClick.bind(this)}>ITALIC</button>
-                            <button onClick={this._onUnderlineClick.bind(this)}>UNDERLINE</button>
-                            <button onClick={this._onCodeClick.bind(this)}>CODE</button>
-                            <button onClick={this._onSelectionDescription.bind(this)}>SELECT FOR DESCRIPTION</button>
-                        </div>
+                    <Col span={20}>
+                        <Input
+                            onChange={this.onURLChange}
+                            ref={"description"}
+                            style={styles.urlInput}
+                            type={"textarea"}
+                            placeholder={'Please enter your description'}
+                            value={this.state.urlValue}
+                            onKeyDown={this.onLinkInputKeyDown}
+                        />
+                    </Col>
+                    <Col span={4}>
+                        <Button onMouseDown={this.confirmLink}>
+                            Confirm
+                        </Button>
                     </Col>
                 </Row>
             </div>
+        }
+        
+        return (
+            <div style={styles.root}>
+                <div style={{marginBottom: 10}}>
+                    Select some text, then use the buttons to add or remove description
+                    on the selected text.
+                </div>
+                <div style={styles.buttons}>
+                    <Button.Group style={{marginRight: 10}}>
+                        <Button onClick={this._onBoldClick.bind(this)}><FaBold size={12} /></Button>
+                        <Button onClick={this._onItalicClick.bind(this)}><FaItalic size={11} /></Button>
+                        <Button onClick={this._onUnderlineClick.bind(this)}><FaUnderline size={12} /></Button>
+                        <Button onClick={this._onCodeClick.bind(this)}><FaCode size={15} /></Button>
+                    </Button.Group>
+
+                    <Button.Group style={{marginRight: 10}}>
+                        <Button onMouseDown={this.promptForLink} type={'primary'}>
+                            Add Description
+                        </Button>
+                        <Button onMouseDown={this.removeLink}>
+                            Remove Description
+                        </Button>
+                    </Button.Group>
+                </div>
+                {urlInput}
+                <Row gutter={8}>
+                    <Col span={12}>
+                        <div style={styles.editor} onClick={this.focus}>
+                            <Editor
+                                editorState={this.state.editorState}
+                                onChange={this.onChange}
+                                placeholder={"Enter some text..."}
+                                ref={"editor"}
+                            />
+                        </div>
+                    </Col>
+                    <Col span={12}>
+                        <div style={styles.editor}>
+                            {this.props.editor.description}
+                        </div>
+                    </Col>
+                </Row>
+                <Button
+                    onClick={this.logState}
+                    style={{marginTop: 10}}
+                >Log State</Button>
+            </div>
         )
     }
-
 }
 
-export default BlockEditor
+
+
+const styles = {
+    root: {
+        //fontFamily: '\'Georgia\', serif',
+        padding: 20,
+        // width: 600,
+    },
+    buttons: {
+        marginBottom: 10,
+    },
+    urlInputContainer: {
+        marginBottom: 10,
+    },
+    urlInput: {
+        //fontFamily: '\'Georgia\', serif',
+        marginRight: 10,
+        padding: 3,
+    },
+    editor: {
+        borderRadius: 2,
+        border: '1px solid #ddd',
+        cursor: 'text',
+        minHeight: 80,
+        padding: 10,
+        boxSizing: 'border-box',
+        boxShadow: 'inset 0px 1px 8px -3px #ABABAB',
+    },
+    button: {
+        marginTop: 10,
+        textAlign: 'center',
+    },
+    link: {
+        color: 'black',
+        backgroundColor: '#ddd',
+        padding: 2,
+        marginBottom: 3,
+        borderRadius: 2
+        // +Math.floor(Math.random()*16777215).toString(16)
+    },
+} 
+
+const mapStateToProps = state => {
+    return { editor: state.editor }
+}
+
+export default connect(mapStateToProps, actions)(BlockEditor)
--- app/components/TestPage.js
+++ app/components/TestPage.js
@@ -1,245 +1,30 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import * as actions from '../actions'
-import { Row, Col } from 'antd'
-import { 
-    CompositeDecorator, 
-    convertToRaw, 
-    Editor, 
-    EditorState, 
-    RichUtils, 
-    ContentState 
-} from 'draft-js'
-import NavBar from '../components/NavBar'
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
+import BlockEditor from './BlockEditor'
 import '../assets/editor.css'
 
 class TestPage extends Component {
     constructor(props) {
         super(props)
-
-        const decorator = new CompositeDecorator([
-            {
-                strategy: this.findLinkEntities,
-                component: this.Link.bind(this),
-            },
-        ])
-
-        this.state = {
-            editorState: EditorState.createEmpty(decorator),
-            showURLInput: false,
-            urlValue: '',
-            description: ''
-        }
-
-        this.focus = () => this.refs.editor.focus()
-        this.onChange = (editorState) => this.setState({editorState})
-        this.logState = () => {
-            const content = this.state.editorState.getCurrentContent()
-            console.log(convertToRaw(content))
-        }
-
-        this.promptForLink = this._promptForLink.bind(this)
-        this.onURLChange = (e) => this.setState({urlValue: e.target.value})
-        this.confirmLink = this._confirmLink.bind(this)
-        this.onLinkInputKeyDown = this._onLinkInputKeyDown.bind(this)
-        this.removeLink = this._removeLink.bind(this)
-
     }
-
-    _promptForLink(e) {
-        e.preventDefault()
-        const {editorState} = this.state
-        const selection = editorState.getSelection()
-        if (!selection.isCollapsed()) {
-            const contentState = editorState.getCurrentContent()
-            const startKey = editorState.getSelection().getStartKey()
-            const startOffset = editorState.getSelection().getStartOffset()
-            const blockWithLinkAtBeginning = contentState.getBlockForKey(startKey)
-            const linkKey = blockWithLinkAtBeginning.getEntityAt(startOffset)
-            let description = ''
-            if (linkKey) {
-                const linkInstance = contentState.getEntity(linkKey)
-                description = linkInstance.getData().description
-            }
-            this.setState({
-                showURLInput: true,
-                urlValue: description,
-            }, () => {
-                setTimeout(() => this.refs.description.focus(), 0)
-            })
-        }
-    }
-
-    _confirmLink(e) {
-        e.preventDefault()
-        const {editorState, urlValue} = this.state
-        const contentState = editorState.getCurrentContent()
-        const contentStateWithEntity = contentState.createEntity(
-            'LINK',
-            'MUTABLE',
-            {description: urlValue}
-        )
-        const entityKey = contentStateWithEntity.getLastCreatedEntityKey()
-        const newEditorState = EditorState.set(editorState, { currentContent: contentStateWithEntity })
-        this.setState({
-        editorState: RichUtils.toggleLink(
-            newEditorState,
-            newEditorState.getSelection(),
-            entityKey
-        ),
-        showURLInput: false,
-        urlValue: '',
-        }, () => {
-            setTimeout(() => this.refs.editor.focus(), 0)
-        })
-    }
-    _onLinkInputKeyDown(e) {
-        if (e.which === 13) {
-            this._confirmLink(e)
-        }
-    }
-    _removeLink(e) {
-        e.preventDefault()
-        const {editorState} = this.state
-        const selection = editorState.getSelection()
-        if (!selection.isCollapsed()) {
-            this.setState({
-                editorState: RichUtils.toggleLink(editorState, selection, null),
-            })
-        }
-    }
-
-    findLinkEntities(contentBlock, callback, contentState) {
-        contentBlock.findEntityRanges(
-            (character) => {
-                const entityKey = character.getEntity() 
-                return (
-                    entityKey !== null &&
-                    contentState.getEntity(entityKey).getType() === 'LINK'
-                ) 
-            },
-            callback
-        ) 
-    }
-
-    Link(props) {
-        const {description} = props.contentState.getEntity(props.entityKey).getData() 
-        return (
-            <code style={styles.link} onMouseOver={() => this.props.changeDescription(description)}>
-                {props.children}
-            </code>
-        ) 
-    }
-
 
     render() {
-        let urlInput
-        if (this.state.showURLInput) {
-        urlInput =
-            <div style={styles.urlInputContainer}>
-                <input
-                    onChange={this.onURLChange}
-                    ref={"description"}
-                    style={styles.urlInput}
-                    type={"text"}
-                    value={this.state.urlValue}
-                    onKeyDown={this.onLinkInputKeyDown}
-                />
-                <button onMouseDown={this.confirmLink}>
-                    Confirm
-                </button>
-            </div>
-        }
-        
         return (
-            <div style={styles.root}>
-                <div style={{marginBottom: 10}}>
-                    Select some text, then use the buttons to add or remove description
-                    on the selected text.
-                </div>
-                <div style={styles.buttons}>
-                    <button
-                        onMouseDown={this.promptForLink}
-                        style={{marginRight: 10}}>
-                        Add Description
-                    </button>
-                    <button onMouseDown={this.removeLink}>
-                        Remove Description
-                    </button>
-                </div>
-                {urlInput}
-                <Row gutter={8}>
-                    <Col span={12}>
-                        <div style={styles.editor} onClick={this.focus}>
-                            <Editor
-                                editorState={this.state.editorState}
-                                onChange={this.onChange}
-                                placeholder={"Enter some text..."}
-                                ref={"editor"}
-                            />
-                        </div>
-                    </Col>
-                    <Col span={12}>
-                        <div style={styles.editor}>
-                            {this.props.editor.description}
-                        </div>
-                    </Col>
-                </Row>
-                <input
-                    onClick={this.logState}
-                    style={styles.button}
-                    type={"button"}
-                    value={"Log State"}
-                />
+            <div>
+                <ReactCSSTransitionGroup
+                    transitionName="example"
+                    transitionAppear={true}
+                    transitionAppearTimeout={500}
+                    transitionEnter={false}
+                    transitionLeave={false}>
+                    <BlockEditor />
+                </ReactCSSTransitionGroup>
             </div>
         )
     }
+
 }
 
-
-
-const styles = {
-    root: {
-        fontFamily: '\'Georgia\', serif',
-        padding: 20,
-        // width: 600,
-    },
-    buttons: {
-        marginBottom: 10,
-    },
-    urlInputContainer: {
-        marginBottom: 10,
-    },
-    urlInput: {
-        fontFamily: '\'Georgia\', serif',
-        marginRight: 10,
-        padding: 3,
-    },
-    editor: {
-        borderRadius: 2,
-        border: '1px solid #ddd',
-        cursor: 'text',
-        minHeight: 80,
-        padding: 10,
-        boxSizing: 'border-box',
-        boxShadow: 'inset 0px 1px 8px -3px #ABABAB',
-    },
-    button: {
-        marginTop: 10,
-        textAlign: 'center',
-    },
-    link: {
-        color: 'black',
-        backgroundColor: '#ddd',
-        padding: 2,
-        marginBottom: 3,
-        borderRadius: 2
-        // +Math.floor(Math.random()*16777215).toString(16)
-    },
-} 
-
-const mapStateToProps = state => {
-    return {editor: state.editor}
-}
-
-export default connect(mapStateToProps, actions)(TestPage)
+export default TestPage
